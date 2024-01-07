f5355d0c1dfa85800008d44ccee50e73a063de6e
Freddy Ramirez <firf.1818@gmail.com>
2024-01-07 16:54:55 -0400

    Primera prueba basica del proyecto

---
+++ code.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('show');
+    var nav = document.querySelector('.user-actions');
+    nav.classList.toggle('show');
+  }
+  
